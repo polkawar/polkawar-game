e1c522293572d67896e44c4192c006a090d4ecfe
Dhruv Sharma <dhruvs8167@gmail.com>
2021-12-13 00:37:38 +0530

    Editor scripts

---
+++ Assets/Scripts/Editor/SimpleEditorUtils.cs
@@ -0,0 +1,61 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+public static class SimpleEditorUtils
+{
+    const string playFromFirstMenuStr = "Utilities/Always Start From Scene 0 &p";
+
+    static bool playFromFirstScene
+    {
+        get { return EditorPrefs.HasKey(playFromFirstMenuStr) && EditorPrefs.GetBool(playFromFirstMenuStr); }
+        set { EditorPrefs.SetBool(playFromFirstMenuStr, value); }
+    }
+
+    [MenuItem(playFromFirstMenuStr, false, 150)]
+    static void PlayFromFirstSceneCheckMenu()
+    {
+        playFromFirstScene = !playFromFirstScene;
+        Menu.SetChecked(playFromFirstMenuStr, playFromFirstScene);
+
+        ShowNotifyOrLog(playFromFirstScene ? "Play from scene 0" : "Play from current scene");
+    }
+
+    // The menu won't be gray out, we use this validate method for update check state
+    [MenuItem(playFromFirstMenuStr, true)]
+    static bool PlayFromFirstSceneCheckMenuValidate()
+    {
+        Menu.SetChecked(playFromFirstMenuStr, playFromFirstScene);
+        return true;
+    }
+
+    // This method is called before any Awake. It's the perfect callback for this feature
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void LoadFirstSceneAtGameBegins()
+    {
+        if (!playFromFirstScene)
+            return;
+
+        if (EditorBuildSettings.scenes.Length == 0)
+        {
+            Debug.LogWarning("The scene build list is empty. Can't play from first scene.");
+            return;
+        }
+
+        foreach (GameObject go in Object.FindObjectsOfType<GameObject>())
+            go.SetActive(false);
+
+        SceneManager.LoadScene(0);
+    }
+
+    static void ShowNotifyOrLog(string msg)
+    {
+        if (Resources.FindObjectsOfTypeAll<SceneView>().Length > 0)
+            EditorWindow.GetWindow<SceneView>().ShowNotification(new GUIContent(msg));
+        else
+            Debug.Log(msg); // When there's no scene view opened, we just print a log
+    }
+}
+
+#endif
